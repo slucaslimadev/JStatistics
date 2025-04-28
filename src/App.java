139241aa11a179669733c9c7a578e2b789e7e7f9
slucaslimadev <lucaslima.rec@gmail.com>
2025-04-28 11:57:11 -0300

    jstatics

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        System.out.println("\t\t--JStatistics--");
+        System.out.print("-- Controle de Dados Empresarial --\t");
+        System.out.print("2025");
+        // ArrayList lista = new ArrayList();
     }
 }
