f50de8454f9fda9780a71b2754c989faae24a311
slucaslimadev <lucaslima.rec@gmail.com>
2025-04-29 11:37:34 -0300

    Finalizando aula logica 1- entrada e saida

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        
+        // Aplicando Lógica de Programação (Aristoteles e George Boolean)
+        Scanner leia = new Scanner(System.in);
+        // Variável = espaço reservado na memória do computador / reserve para guardar valores
+        // Tipos de variáveis: text, int, boolean
+        /*
+         * EX-1: CRIE UM ALGORITMO PARA O JSTATISTICS QUE LEIA A OPÇÃO DESEJADA:
+         * QUAL O CONJUNTO DE DADOS UTILIZADOS: 1- POPULACIONAL, 2- AMOSTRAL
+         * O VALOR RECEBIDO DEVE SER INTEIRO
+         */
+        JOptionPane.showMessageDialog(null, "O Jogo da Estatística: JStatistics");
+        String opcaoDesejado = JOptionPane.showInputDialog(null,"Vamos começar o Jogo?\nEscolha a estratégia:\n1- Populacional\n2- Amostral");
+        int opDesejado = Integer.parseInt(opcaoDesejado);
+        JOptionPane.showMessageDialog(null, "Você escolheu:"+opDesejado);
+
+        /*
+         * EX-2: VOCÊ DEVE CRIAR UM MENU PERGUNTANDO PARA O USUÁRIO:
+         * QUAL TIPO DE VARIÁVEL ELE DESEJA CADASTRAR:
+         * 1- QUANTITATIVA
+         * 2- QUALITATIVA
+         * OS MENUS DEVE TER AS QUEBRAS DE LINHA CONFORME O TEXTO ACIMA
+         * MOSTRE A OPÇÃO ESCOLHIDA (INT)
+         */
+        System.out.println("-----------------------------------------");
+        String opcaoVariavel = JOptionPane.showInputDialog(null,"Qual tipo de variável você deseja cadastrar?\n1- Quantitativa\n2- Qualitativa");
+        int opVariavel = Integer.parseInt(opcaoVariavel);
+        JOptionPane.showMessageDialog(null, "Sua escolha foi:"+opVariavel);
+
     }
 }
