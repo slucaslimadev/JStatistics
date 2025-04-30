e4ef6bd7817df1d192f9a762d7357833c17cbe00
slucaslimadev <lucaslima.rec@gmail.com>
2025-04-30 09:01:40 -0300

    ajustei algumas informações

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         // Aplicando Lógica de Programação (Aristoteles e George Boolean)
-        Scanner leia = new Scanner(System.in);
+        
         // Variável = espaço reservado na memória do computador / reserve para guardar valores
         // Tipos de variáveis: text, int, boolean
         /*
@@ -13,10 +13,16 @@ public class App {
          * QUAL O CONJUNTO DE DADOS UTILIZADOS: 1- POPULACIONAL, 2- AMOSTRAL
          * O VALOR RECEBIDO DEVE SER INTEIRO
          */
+        /*Scanner leia = new Scanner(System.in);
+        int opcao; //declarei a variável em Java.
+        System.out.println("\t #------- \t #------ \t JStatistics \t #----- \t #------ \t ");
+        System.out.println("Qual o conjunto de dados utilizado: 1- populacional 2-amostral");
+        opcao = leia.nextInt();
+        System.out.println(opcao);*/
+        int opcao;
         JOptionPane.showMessageDialog(null, "O Jogo da Estatística: JStatistics");
-        String opcaoDesejado = JOptionPane.showInputDialog(null,"Vamos começar o Jogo?\nEscolha a estratégia:\n1- Populacional\n2- Amostral");
-        int opDesejado = Integer.parseInt(opcaoDesejado);
-        JOptionPane.showMessageDialog(null, "Você escolheu:"+opDesejado);
+        opcao = Integer.parseInt(JOptionPane.showInputDialog("Vamos começar o Jogo? \n Escolha a estratégia: \n 1- Populacional \n 2- Amostral"));
+        JOptionPane.showMessageDialog(null, "Você escolheu: "+opcao,"JStatistics",JOptionPane.DEFAULT_OPTION);
 
         /*
          * EX-2: VOCÊ DEVE CRIAR UM MENU PERGUNTANDO PARA O USUÁRIO:
@@ -26,10 +32,10 @@ public class App {
          * OS MENUS DEVE TER AS QUEBRAS DE LINHA CONFORME O TEXTO ACIMA
          * MOSTRE A OPÇÃO ESCOLHIDA (INT)
          */
-        System.out.println("-----------------------------------------");
-        String opcaoVariavel = JOptionPane.showInputDialog(null,"Qual tipo de variável você deseja cadastrar?\n1- Quantitativa\n2- Qualitativa");
+        String opcaoVariavel = JOptionPane.showInputDialog(null,
+        "Que tipo de variável ele deseja cadastrar:\n 1- Quantitativa \n 2- Qualitativa");
         int opVariavel = Integer.parseInt(opcaoVariavel);
-        JOptionPane.showMessageDialog(null, "Sua escolha foi:"+opVariavel);
+        JOptionPane.showMessageDialog(null,"Você escolhe: "+opVariavel,"JStatistics",JOptionPane.INFORMATION_MESSAGE);
 
     }
 }
