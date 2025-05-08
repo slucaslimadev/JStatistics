2007dd6fbdc2ec9d5b47e59f88476be4ecb95daa
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-08 11:35:30 -0300

    AULA 6

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Git!");
+        // Classe é um conjunto de objetos
+        // Nosso objetivo é entender a diferença entre método void, método string
+        // Vaca
+        Mamifero m1 = new Mamifero();
+        m1.setNomeCientifico("Bos taurus");
+        JOptionPane.showMessageDialog(null,"Nome Cientifico: "+m1.getNomeCientifico());
+        //Considerando a utilização de metodos set e get
+        //Desafio
+        // Crie mais dois objetos: Armanezar e mostrar os dados: canguru, peixe boi
+        // Canguru
+        Mamifero m2 = new Mamifero();
+        m2.setNomeCientifico("Macropus");
+        JOptionPane.showMessageDialog(null, "Nome Cientifico: "+m2.getNomeCientifico());
+        // Peixe boi
+        Mamifero m3 = new Mamifero();
+        m3.setNomeCientifico("Trichechus");
+        JOptionPane.showMessageDialog(null, "Nome Cientifico: "+m3.getNomeCientifico());
+
     }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,18 @@
+public class Mamifero {
+    // Atributos 
+    private int id ;
+    private String nomeCientifico;
+
+    public int getId(){
+        return this.id;
+    }
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+
+    }
+    public String getNomeCientifico(){
+        return this.nomeCientifico;
+    }
+
+
+}
