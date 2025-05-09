ded429ef62efafa9997e6c4f0ed925110582f2c7
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-09 08:56:08 -0300

    exercicio 2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-    // Exercicio 1: Alô, Jardim! - Exibe mensagem de boas-vindas com JOptionPane.
-    JOptionPane.showMessageDialog(null, "Aló, Jardim!");
+    // Exercicio 2: Cadastro simples de cliente - Entrada e exibição do nome e endereço.
+    String nome;
+    String endereco;
+    JOptionPane.showMessageDialog(null, "Jardim Verde: Sistema de controle de Jardinagem");
+    JOptionPane.showMessageDialog(null, "Cadastro de cliente");
+    nome = JOptionPane.showInputDialog("Digite seu nome: ");
+    endereco = JOptionPane.showInputDialog("Digite seu endereço: ");
+    JOptionPane.showMessageDialog(null, "Dados cadastrados:\nNome: "+nome+"\nEndereço: "+endereco, "Jardim Verde", JOptionPane.DEFAULT_OPTION);
+
 
     }
 }
