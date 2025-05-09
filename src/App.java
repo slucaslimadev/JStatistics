8edda0739825ee55287021485ca2b413ea9c26c3
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-09 09:17:20 -0300

    ex4

--- src/App.java
+++ src/App.java
@@ -11,6 +11,15 @@ public class App {
     Double calcArea = (largura * comprimento);
     JOptionPane.showMessageDialog(null, "Largura: "+largura+"\nComprimento: "+comprimento+"\nCalculo da Área: "+calcArea);
 
+    // Nível 2 - Estrutura de Seleção
+    //Verificar tamanho do jardim - Classifica como Grande ou Pequeno.
+
+    if(calcArea <= 20){
+        JOptionPane.showMessageDialog(null, "Jardim cadastrado é Pequeno");
+    }else {
+        JOptionPane.showMessageDialog(null, "Jardim cadastrado é grande");
+    }
+
 
     }
 }
