5b94fbd0687c7d60233c1bba8dd30ee709ff20f7
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-09 08:38:20 -0300

    exercicio 1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
+    // Exercicio 1: Alô, Jardim! - Exibe mensagem de boas-vindas com JOptionPane.
+    JOptionPane.showMessageDialog(null, "Aló, Jardim!");
     }
 }
