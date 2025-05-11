c8d3df0c8ee161abfa5bec3cdac4458ad626df68
Lucas dos Santos <lucaslima.rec@gmail.com>
2025-05-11 08:40:38 -0300

    ex9

--- src/App.java
+++ src/App.java
@@ -1,10 +1,26 @@
-import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\t\t--JStatistics--");
-        System.out.print("-- Controle de Dados Empresarial --\t");
-        System.out.print("2025");
-        // ArrayList lista = new ArrayList();
+       // 8. Média da área dos jardins - Calcula a média de N áreas.
+       int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade de Áreas que você Deseja calcular"));
+       double someAreas = 0;
+       double Area = 0;
+       for (int i = 1; i<= n; i++){
+        Area = Double.parseDouble(JOptionPane.showInputDialog("Digite a Área do seu jardim: "+ i));
+        someAreas+=Area;
+
+
     }
+    double mediaArea = someAreas / n;
+    JOptionPane.showMessageDialog(null, "\nA média da área dos " + n + " jardins é: " + mediaArea);
+    if(Area > 100){
+        JOptionPane.showMessageDialog(null,"\nVocê tem: "+ n + " jardins com área maior que 100 m²");
+
+    } 
+    else{
+        JOptionPane.showMessageDialog(null,"\nVocê tem: "+ n + " jardins com área menor que 100 m²");
+    }
+}
 }
