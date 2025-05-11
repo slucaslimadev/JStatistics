e2a5ba7b9232bbd7e8e930411e5ea40cd0b0e84d
Lucas dos Santos <lucaslima.rec@gmail.com>
2025-05-11 09:29:26 -0300

    ex12

--- src/App.java
+++ src/App.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Nível 4 - Vetores Básicos
-       // 10. Armazenar áreas em vetor - Armazena e exibe áreas.
-       int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de Áreas: "));
-       double[] areas = new double[n];
-         for (int i = 0; i < n; i++) {
-              areas[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite a área " + (i + 1) + ": "));
-              
-         }
+        // 12. Ordenar serviços por valor - Ordena vetor de valores.
+        int numServicos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de serviços:"));
+        // Arrays
+        double[] valores = new double[numServicos];
+        for(int i=0; i < numServicos; i++){
+            valores[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do serviço " + (i+1) + ":"));
+        }
+
+        Arrays.sort(valores);
+        JOptionPane.showMessageDialog(null, "Valores ordenados: " + Arrays.toString(valores));
+
+
+    }
+
 
 
 }
-}
+
