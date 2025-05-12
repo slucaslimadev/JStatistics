3d8fdabb15c3268dd20ebdbbcf471e31bee85b59
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-12 08:21:16 -0300

    ex9

--- src/App.java
+++ src/App.java
@@ -6,17 +6,22 @@ public class App {
        // 8. Média da área dos jardins - Calcula a média de N áreas.
        int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade de Áreas que você Deseja calcular"));
        double someAreas = 0;
+       double Area = 0;
        for (int i = 1; i<= n; i++){
-        double Area = Double.parseDouble(JOptionPane.showInputDialog("Digite a Área do seu jardim: "+ i));
+        Area = Double.parseDouble(JOptionPane.showInputDialog("Digite a Área do seu jardim: "+ i));
         someAreas+=Area;
 
 
     }
+    // 9. Contar quantos jardins são grandes - Conta os jardins com área > 100m2.
     double mediaArea = someAreas / n;
     JOptionPane.showMessageDialog(null, "\nA média da área dos " + n + " jardins é: " + mediaArea);
-    if(Area >= 100.0){
-        
-    }
+    if(Area > 100){
+        JOptionPane.showMessageDialog(null,"\nVocê tem: "+ n + " jardins com área maior que 100 m²");
 
+    } 
+    else{
+        JOptionPane.showMessageDialog(null,"\nVocê tem: "+ n + " jardins com área menor que 100 m²");
     }
+}
 }
