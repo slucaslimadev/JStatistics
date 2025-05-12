af16f9e20f98a6b765644d738b90a754d6eecd1d
slucaslimadev <lucaslima.rec@gmail.com>
2025-05-12 10:31:30 -0300

    ex18

---
+++ src/Servico.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Servico {
+    public static void main(String[] args) throws Exception {
+        // 18. Classe Serviço com aplicarDesconto() - Retorna valor com desconto.
+        
+
+    }
+}
